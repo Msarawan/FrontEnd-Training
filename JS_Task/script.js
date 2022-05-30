4fef207fdee8f8ee02eb3294e130a08ee8c53937
Marjina Sarawan <msarawan@redhat.com>
2022-05-30 23:51:16 +0530

    JS Task added

---
+++ JS_Task/script.js
@@ -0,0 +1,36 @@
+
+(function () {
+    "use strict";
+     var list = document.querySelectorAll(".timeline li");
+  
+    function elementDisplay(el) {
+      var rect = el.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <=
+          (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
+  
+    function callElement() {
+      for (var i = 0; i < list.length; i++) {
+        if (elementDisplay(list[i])) {
+          list[i].classList.add("in-view");
+        }
+      }
+    }
+  
+    window.addEventListener("load", callElement);
+    window.addEventListener("resize", callElement);
+    window.addEventListener("scroll", callElement);
+  })();
+  
+
+  //  SetTimeout function for size of img change
+  const img = document.querySelector("img");
+    setTimeout(() => {
+       img.style.width="350px";
+    },2000)
+  
